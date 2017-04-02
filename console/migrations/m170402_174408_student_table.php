5cb6b9ea8daf9c793d591fd893d90ddb94470cc9
Mostafa Yehia <Mostafa Yehia>
2017-04-02 22:06:41 +0200

    migration

---
+++ console/migrations/m170402_174408_student_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+class m170402_174408_student_table extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('student',[
+            'id' => $this->primarykey(),
+            'name' => $this->string(12)->notNull(),
+            'password' => $this->integer()->notNull(),
+            'gpa' => $this->float()->notNull()
+        ]);
+        $this->alterColumn('{{%student}}', 'id', $this->primarykey() .' NOT NULL AUTO_INCREMENT');
+    }
+    
+
+
+    public function safeDown()
+    {
+        echo "m170402_174408_student_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
